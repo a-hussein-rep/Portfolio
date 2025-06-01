368817d81060cff1c038818edfd9b58b110ad4ec
a-hussein-rep <ahmad.hussein.social@gmail.com>
2025-06-01 17:52:50 +0200

    add basic authentication to craete action

--- Portfolio/PersonalBlog/PersonalBlog/PersonalBlog/Controllers/HomeController.cs
+++ Portfolio/PersonalBlog/PersonalBlog/PersonalBlog/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using PersonalBlog.Database;
 using PersonalBlog.Models;
 using System.Diagnostics;
+using System.Text;
 
 namespace PersonalBlog.Controllers;
 
@@ -19,6 +20,9 @@ public class HomeController : Controller
 
     public async Task<IActionResult> Index()
     {
+        var config = HttpContext.RequestServices.GetService(typeof(IConfiguration)) as IConfiguration;
+        ViewBag.IsAdmin = IsAuthorized(HttpContext, config);
+
         return View(await dbContext.Articles.ToListAsync());
     }
 
@@ -32,4 +36,21 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    private bool IsAuthorized(HttpContext context, IConfiguration config)
+    {
+        var authHeader = context.Request.Headers["Authorization"].ToString();
+        var username = config["AdminAuth:Username"];
+        var password = config["AdminAuth:Password"];
+
+        if (authHeader?.StartsWith("Basic ") == true)
+        {
+            var encoded = authHeader["Basic ".Length..].Trim();
+            var decoded = Encoding.UTF8.GetString(Convert.FromBase64String(encoded));
+            var parts = decoded.Split(':');
+            return parts.Length == 2 && parts[0] == username && parts[1] == password;
+        }
+        return false;
+    }
+
 }
---
+++ Portfolio/PersonalBlog/PersonalBlog/PersonalBlog/Internal/BasicAuthAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text;
+
+namespace PersonalBlog.Internal;
+
+public class BasicAuthAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var config = context.HttpContext.RequestServices.GetService(typeof(IConfiguration)) as IConfiguration;
+        var username = config["AdminAuth:Username"];
+        var password = config["AdminAuth:Password"];
+
+        var authHeader = context.HttpContext.Request.Headers.Authorization.ToString();
+
+        if (authHeader != null && authHeader.StartsWith("Basic "))
+        {
+            var encodedUsernamePassword = authHeader["Basic ".Length..].Trim();
+            var decodedUsernamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
+            var parts = decodedUsernamePassword.Split(':');
+
+            if (parts.Length == 2 && parts[0] == username && parts[1] == password)
+            {
+                return; // Authorized
+            }
+        }
+
+        context.Result = new UnauthorizedResult();
+        context.HttpContext.Response.Headers["WWW-Authenticate"] = "Basic realm=\"Admin Area\"";
+    }
+}
