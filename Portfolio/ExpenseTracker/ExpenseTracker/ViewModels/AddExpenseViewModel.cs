0b61f9b0fd3debf9276b8a8639526987cacc67d8
a-hussein-rep <ahmad.hussein.social@gmail.com>
2025-06-12 23:43:12 +0200

    init commit - expense tracker app using wpf

---
+++ Portfolio/ExpenseTracker/ExpenseTracker/ViewModels/AddExpenseViewModel.cs
@@ -0,0 +1,71 @@
+﻿using ExpenseTracker.Helpers;
+using ExpenseTracker.Models;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ExpenseTracker.ViewModels;
+
+public class AddExpenseViewModel : PropertyChangedBase
+{
+    public string Description { get; set; }
+
+    public string Amount { get; set; }
+
+    public DateTime Date { get; set; } = DateTime.Now;
+
+    public string Category { get; set; }
+
+    public ICommand SaveCommand { get; }
+
+    public ICommand CancelCommand { get; }
+
+    public ObservableCollection<string> Categories { get; private set; }
+
+
+    public event Action<ExpenseModel> ExpenseAdded;
+
+    public AddExpenseViewModel()
+    {
+        SaveCommand = new RelayCommand(SaveExpense, CanSave);
+
+        CancelCommand = new RelayCommand(CancelAddExpense);
+
+        Categories = new ObservableCollection<string>
+        {
+            "Food",
+            "Transport",
+            "Utilities",
+            "Entertainment",
+            "Health",
+            "Other"
+        };
+    }
+
+    private bool CanSave(object? obj)
+    {
+        return !string.IsNullOrWhiteSpace(Amount) && !string.IsNullOrWhiteSpace(Category);
+    }
+
+    private void SaveExpense(object? obj)
+    {
+        var newExpense = new ExpenseModel
+        {
+            Description = Description,
+            Amount = decimal.Parse(Amount, NumberStyles.Number, new CultureInfo("de-DE")),
+            Date = Date,
+            Category = Category
+        };
+
+        ExpenseAdded?.Invoke(newExpense);
+
+        (obj as Window)?.Close();
+    }
+
+    private void CancelAddExpense(object? obj)
+    {
+        (obj as Window)?.Close();
+    }
+}
---
+++ Portfolio/ExpenseTracker/ExpenseTracker/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+using ExpenseTracker.Helpers;
+using ExpenseTracker.Models;
+using ExpenseTracker.Views;
+
+namespace ExpenseTracker.ViewModels;
+
+public class MainViewModel : PropertyChangedBase
+{
+    public ObservableCollection<ExpenseModel> Expenses { get; }
+
+    public ICommand AddExpenseCommand { get; }
+    
+    public MainViewModel()
+    {
+        Expenses = new();
+        
+        AddExpenseCommand = new RelayCommand(OpenAddExpenseWindow);
+    }
+
+    private void OpenAddExpenseWindow(object? obj)
+    {
+        var addExpenseView = new AddExpenseView();
+
+        if (addExpenseView.DataContext is AddExpenseViewModel addExpenseViewModel)
+        {
+            addExpenseViewModel.ExpenseAdded += expense => Expenses.Add(expense);
+        }
+
+        addExpenseView.ShowDialog();
+    }
+}
